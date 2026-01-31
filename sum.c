b64d6754ab2d900b624c630413985fcd30a40a34
drishya06-mp <drishyarajeev06@gmail.com>
2026-01-31 10:46:46 +0530

    Update sum.c
    I removed the new line from print statement.

--- sum.c
+++ sum.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
 	int a,b,result;
-	printf("Enter two numbers\n");
+	printf("Enter two numbers:");
 	scanf("%d%d",&a,&b);
 	result=a+b;
 	printf("The sum is %d",result);
